599f18db721a1eebf3123a171f4da9916ae54213
A. Taha Baki <a.taha.baki@outlook.com>
2021-02-08 02:33:48 +0300

    now it logs in...

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const puppeteer = require('puppeteer')
-const dotenv = require('dotenv')
-
+const dotenv = require('dotenv').config();
+const puppeteer = require('puppeteer');
 
 (async () => {
-})()
+  const browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 150
+	});
+  const page = await browser.newPage();
+	await page.goto('https://wallet.uphold.com/login');
+
+	await page.type('input[type="email"]',process.env.EMAIL);
+	await page.type('input[type="password"]',process.env.PASSWORD);
+	await page.click('button[type="submit"]');
+
+	debugger;
+  //await browser.close();
+})();
